8f1115947af6fb6d9a8e0dbcc6316e545dcfb647
ThomasKluiters <tmkluiters@student.tudelft.nl>
2017-06-30 20:28:04 +0200

    Add (empty) SpoofaxLanguageServerTest test class

---
+++ src/test/kotlin/org/metaborg/spoofax/lsp/SpoofaxLanguageServerTest.kt
@@ -0,0 +1,24 @@
+package org.metaborg.spoofax.lsp
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+/**
+ * Test cases for [SpoofaxLanguageServer]
+ */
+internal class SpoofaxLanguageServerTest {
+
+    @Test
+    fun shutdownTest() {
+    }
+
+    @Test
+    fun exitTest() {
+    }
+
+    @Test
+    fun connectTest() {
+    }
+
+}
